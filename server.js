a9a4f765056f23a729c2136643517e70402aa78e
yogito-creta <nuriyeaksakal19@gmail.com>
2026-01-20 16:24:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,31 +6,35 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Railway bunu SEVİYOR
+app.get("/", (req, res) => {
+  res.send("Pixel site ayakta lan 😎");
+});
+
 app.use(express.static("public"));
 
 const WIDTH = 50;
 const HEIGHT = 30;
 
-// Canvas verisi (sunucuda tutulur)
 let pixels = Array.from({ length: HEIGHT }, () =>
   Array.from({ length: WIDTH }, () => "#ffffff")
 );
 
 io.on("connection", (socket) => {
-  console.log("Biri girdi lan");
+  console.log("Biri girdi");
 
-  // Yeni girene tüm canvas gönder
   socket.emit("init", pixels);
 
   socket.on("draw", ({ x, y, color }) => {
-    if (pixels[y] && pixels[y][x]) {
+    if (pixels[y] && pixels[y][x] !== undefined) {
       pixels[y][x] = color;
       socket.broadcast.emit("draw", { x, y, color });
     }
   });
 });
 
+// 🔴 EN KRİTİK SATIRLAR
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log("Server ayakta:", PORT);
 });
