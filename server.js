eae69960bce88d82f59a1b11ade05a02b6d522e2
yogito-creta <nuriyeaksakal19@gmail.com>
2026-01-24 21:54:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,28 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Dosyalar klasörde olmadığı için direkt ana dizini sunuyoruz
-app.use(express.static(__dirname));
+// Dosyaların bulunduğu yer (Ana dizin)
+const publicPath = path.resolve(__dirname);
+app.use(express.static(publicPath));
+
+// Eğer birisi ana sayfaya gelirse index.html gönder
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 let canvasData = {}; 
 
 io.on('connection', (socket) => {
     socket.emit('init-canvas', canvasData);
-
     socket.on('draw-pixel', (data) => {
         canvasData[`${data.x}-${data.y}`] = data.color;
         socket.broadcast.emit('update-pixel', data);
     });
-
     socket.on('chat-message', (data) => {
         io.emit('chat-message', data);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Yogito Pixel Aktif: ${PORT}`));
-                
+server.listen(PORT, () => console.log(`Sunucu aktif: ${PORT}`));
+    
