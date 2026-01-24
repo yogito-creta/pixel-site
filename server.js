fa1ba23c5144be4ebec94f646c6be08254aecf82
yogito-creta <nuriyeaksakal19@gmail.com>
2026-01-24 22:07:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,30 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(__dirname));
-
-// Verileri RAM'de tutuyoruz
-let canvasData = {}; 
-let chatHistory = [];
-
-io.on('connection', (socket) => {
-    // Yeni bağlanana mevcut durumu gönder
-    socket.emit('init-canvas', canvasData);
-    socket.emit('init-chat', chatHistory);
-
-    // Pixel çizildiğinde
-    socket.on('draw-pixel', (data) => {
-        const key = `${data.x},${data.y}`;
-        canvasData[key] = data.color;
-        // Çizeni hariç tutarak herkese gönder
-        socket.broadcast.emit('update-pixel', data);
-    });
-
-    // Mesaj geldiğinde
-    socket.on('chat-message', (data) => {
-        const msg = { user: data.user || 'Oyuncu', text: data.text };
-        chatHistory.push(msg);
-        if (chatHistory.length > 50) chatHistory.shift(); // Son 50 mesajı tut
-        io.emit('chat-message', msg);
-    });
-});
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Yogito Pixel Aktif: ${PORT}`));
+
+let pixels = {};
+
+app.use(express.json());
+
+// index.html'i ROOT'tan servis et (public yok!)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// pixel verilerini ver
+app.get("/pixels", (req, res) => {
+  res.json(pixels);
+});
+
+// pixel kaydet
+app.post("/pixel", (req, res) => {
+  const { x, y, color } = req.body;
+  pixels[`${x},${y}`] = color;
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => {
+  console.log("Server ayakta:", PORT);
+});
